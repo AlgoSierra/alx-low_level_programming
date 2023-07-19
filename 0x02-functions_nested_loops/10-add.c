26d32d9e498b073b142b58374c5e86d1636282d5
AlgoSierra <nangiroevanstioko@gmail.com>
2023-07-19 05:53:20 -0700

    10. a + b

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - function that adds two integers and returns the result.
+ * @a: first integer
+ * @b: second integer
+ *
+ * Return: Always 0 (Success).
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
