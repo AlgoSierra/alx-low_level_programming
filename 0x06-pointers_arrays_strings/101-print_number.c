09f77a6da137295349c18e0eead001f06563f502
AlgoSierra <nangiroevanstioko@gmail.com>
2023-07-31 01:41:14 -0700

    9. Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -8,18 +8,15 @@
  */
 void print_number(int n)
 {
-	int i;
-
 	if (n < 0)
 	{
 		_putchar('-');
-		n *= -1;
+		n = -n;
 	}
-	i = n;
 
-	if (i / 10)
+	if (n / 10 != 0)
 	{
-		print_number(i / 10);
+		print_number(n / 10);
 	}
-	_putchar(i % 10 + '0');
+	_putchar('0' + n % 10);
 }
