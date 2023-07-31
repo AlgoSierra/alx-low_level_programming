4a468f9906b6d0340801f70cd0a8671aec885e22
AlgoSierra <nangiroevanstioko@gmail.com>
2023-07-30 23:27:03 -0700

    1. strncat

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -5,7 +5,7 @@
  * @dest: destination string
  * @src: source string
  * @n: number of bytes
- * 
+ *
  * Return: pointer to the resulting string dest
  */
 char *_strncat(char *dest, char *src, int n)
