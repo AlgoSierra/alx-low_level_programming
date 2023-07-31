1767b7872d319b319113852619a1f3f7b24256ea
AlgoSierra <nangiroevanstioko@gmail.com>
2023-07-30 23:46:44 -0700

    2. strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -2,25 +2,25 @@
 
 /**
  * _strncpy - function that copies a string
- * @dest: destination
- * @src: source
- * @n: number of bytes
+ * @dest: destination buffer
+ * @src: source string
+ * @n: maximum number of characters to copy
  *
- * Return: dest.
+ * Return: pointer to the resulting 'dest' string
  */
 char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	i = 0;
-	while (i < n && *(src + i))
+	for (i = 0; i < n && src[i] != '\0'; i++)
 	{
-		*(dest + i) = '\0';
-		i++;
+		dest[i] = src[i];
 	}
+	/* If 'src' has fewer than 'n' characters, */
+	/* pad the remaining 'dest' with null bytes */
 	while (i < n)
 	{
-		*(dest + i) = '\0';
+		dest[i] = '\0';
 		i++;
 	}
 	return (dest);
