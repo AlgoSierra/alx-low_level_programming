cd90f31578d1c9e7bff40e62c50d325a4af422b0
AlgoSierra <nangiroevanstioko@gmail.com>
2023-08-30 02:29:31 -0700

    0. Print list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_list - function that prints all elements of a linked list
+ * list_t - (structure representing the head of a singley linked list) and
+ * @h: pointer to the list_t to print
+ *
+ * Return: the number of nodes in the linked list
+ */
+
+size_t print_list(const list_t *h)
+{
+	/*Initialize a counter for the number of nodes */
+	size_t count = 0;
+
+	while (h)
+	{
+		if (h->str)
+		{
+			printf("[%u] %s\n", h->len, h->str);
+		}
+		else
+		{
+			printf("[0] (nil)\n");
+		}
+
+		count++;
+		h = h->next;
+	}
+
+	return (count);
+}
