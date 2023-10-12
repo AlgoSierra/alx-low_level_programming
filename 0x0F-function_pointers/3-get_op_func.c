5124f9ff409afd5e4dcb87d78b63562d6fb3b682
AlgoSierra <nangiroevanstioko@gmail.com>
2023-10-12 03:13:32 -0700

    3. A goal is not always meant to be reached, it often serves simply as something to aim at

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,17 +1,12 @@
-#include "3-calc.h"
-#include <stddef.h>
-
+#include"3-calc.h"
 /**
- * get_op_func - function that selects the correct function
- * to perform the operation asked by the user
- * function returns a pointer to the function that
- * corresponds to the operator given as a parameter
- *
- * @s: operator passed as argument to the program
- *
- * Return: If s does not match any of the 5 expected operators (+, -, *, /, %),
- * return NULL
- */
+ * get_op_func - get the operator and redirect to the func.
+ * @s: the operator
+(* a blank line
+* Description: get the operator and redirect to the func.)?
+(* section header: 3-calc.h)*
+* Return: return null or the function to do.
+*/
 
 int (*get_op_func(char *s))(int, int)
 {
@@ -23,13 +18,16 @@ int (*get_op_func(char *s))(int, int)
 		{"%", op_mod},
 		{NULL, NULL}
 	};
-	int j = 0;
+	int i;
 
-	while (ops[j].op)
+	i = 0;
+	while (i < 5)
 	{
-		if (*s == *ops[j].op)
-			return (ops[j].f);
-		j++;
+		if (strcmp(s, ops[i].op) == 0)
+		{
+			return (*(ops[i]).f);
+		}
+		i++;
 	}
 	return (NULL);
 }
